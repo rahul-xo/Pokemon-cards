d4b0772d9b19a1a5177891324bbbc2402a3d4e74
Rahul Bisht <rahulbisht8886@gmail.com>
2025-05-14 22:46:15 +0530

    modified

--- src/Api/FetchApi.jsx
+++ src/Api/FetchApi.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 
-const FetchApi = ({setData}) => {
+const FetchApi = ({ setData }) => {
   const API = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-  useEffect(() => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((output) => setData(output))
-      .catch((err) => console.log(err));
+
+  const apiCall=async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      setData(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  useEffect(()=>{
+    apiCall();
   }, []);
 };
 
